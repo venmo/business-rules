ac02040db13deae3dbd18149cd94110f31caab1d
Danny Cosson <dcosson@gmail.com>
2014-04-02 13:49:10 -0400

    Implement do_actions

--- tests/test_engine_logic.py
+++ tests/test_engine_logic.py
@@ -1,9 +1,9 @@
 from business_rules import engine
-from business_rules.variables import BaseVariables, rule_variable, TYPE_STRING, TYPE_NUMERIC
+from business_rules.variables import BaseVariables
 from business_rules.operators import StringType
 from business_rules.actions import BaseActions
 
-from mock import MagicMock
+from mock import patch, MagicMock
 from unittest2 import TestCase
 
 
@@ -13,7 +13,8 @@ class EngineTests(TestCase):
     ### Run
     ###
 
-    def test_run_all_some_rule_triggered(self):
+    @patch.object(engine, 'run')
+    def test_run_all_some_rule_triggered(self, *args):
         """ By default, does not stop on first triggered rule. Returns True if
         any rule was triggered, otherwise False
         """
@@ -22,7 +23,6 @@ class EngineTests(TestCase):
         variables = BaseVariables()
         actions = BaseActions()
 
-        engine.run = MagicMock()
         def return_action1(rule, *args, **kwargs):
             return rule['actions'] == 'action name 1'
         engine.run.side_effect = return_action1
@@ -32,34 +32,31 @@ class EngineTests(TestCase):
         self.assertEqual(engine.run.call_count, 2)
 
         # switch order and try again
-        engine.run = MagicMock()
-        engine.run.side_effect = return_action1
+        engine.run.reset_mock()
 
         result = engine.run_all([rule2, rule1], variables, actions)
         self.assertTrue(result)
         self.assertEqual(engine.run.call_count, 2)
 
-
-    def test_run_all_stop_on_first(self):
+    @patch.object(engine, 'run', return_value=True)
+    def test_run_all_stop_on_first(self, *args):
         rule1 = {'conditions': 'condition1', 'actions': 'action name 1'}
         rule2 = {'conditions': 'condition2', 'actions': 'action name 2'}
         variables = BaseVariables()
         actions = BaseActions()
 
-        engine.run = MagicMock(return_value=True)
         result = engine.run_all([rule1, rule2], variables, actions,
                 stop_on_first_trigger=True)
         self.assertEqual(result, True)
         self.assertEqual(engine.run.call_count, 1)
         engine.run.assert_called_once_with(rule1, variables, actions)
 
-    def test_run_that_triggers_rule(self):
+    @patch.object(engine, 'check_conditions_recursively', return_value=True)
+    @patch.object(engine, 'do_actions')
+    def test_run_that_triggers_rule(self, *args):
         rule = {'conditions': 'blah', 'actions': 'blah2'}
         variables = BaseVariables()
         actions = BaseActions()
-
-        engine.check_conditions_recursively = MagicMock(return_value=True)
-        engine.do_actions = MagicMock()
 
         result = engine.run(rule, variables, actions)
         self.assertEqual(result, True)
@@ -68,13 +65,12 @@ class EngineTests(TestCase):
         engine.do_actions.assert_called_once_with(rule['actions'], actions)
 
 
-    def test_run_that_doesnt_trigger_rule(self):
+    @patch.object(engine, 'check_conditions_recursively', return_value=False)
+    @patch.object(engine, 'do_actions')
+    def test_run_that_doesnt_trigger_rule(self, *args):
         rule = {'conditions': 'blah', 'actions': 'blah2'}
         variables = BaseVariables()
         actions = BaseActions()
-
-        engine.check_conditions_recursively = MagicMock(return_value=False)
-        engine.do_actions = MagicMock()
 
         result = engine.run(rule, variables, actions)
         self.assertEqual(result, False)
@@ -83,11 +79,10 @@ class EngineTests(TestCase):
         self.assertEqual(engine.do_actions.call_count, 0)
 
 
-    def test_check_all_conditions_with_all_true(self):
+    @patch.object(engine, 'check_condition', return_value=True)
+    def test_check_all_conditions_with_all_true(self, *args):
         conditions = {'all': [{'thing1': ''}, {'thing2': ''}]}
         variables = BaseVariables()
-
-        engine.check_condition = MagicMock(return_value=True)
 
         result = engine.check_conditions_recursively(conditions, variables)
         self.assertEqual(result, True)
@@ -99,11 +94,10 @@ class EngineTests(TestCase):
     ###
     ### Check conditions
     ###
-    def test_check_all_conditions_with_all_false(self):
+    @patch.object(engine, 'check_condition', return_value=False)
+    def test_check_all_conditions_with_all_false(self, *args):
         conditions = {'all': [{'thing1': ''}, {'thing2': ''}]}
         variables = BaseVariables()
-
-        engine.check_condition = MagicMock(return_value=False)
 
         result = engine.check_conditions_recursively(conditions, variables)
         self.assertEqual(result, False)
@@ -115,22 +109,20 @@ class EngineTests(TestCase):
             engine.check_conditions_recursively({'all': []}, BaseVariables())
 
 
-    def test_check_any_conditions_with_all_true(self):
+    @patch.object(engine, 'check_condition', return_value=True)
+    def test_check_any_conditions_with_all_true(self, *args):
         conditions = {'any': [{'thing1': ''}, {'thing2': ''}]}
         variables = BaseVariables()
-
-        engine.check_condition = MagicMock(return_value=True)
 
         result = engine.check_conditions_recursively(conditions, variables)
         self.assertEqual(result, True)
         engine.check_condition.assert_called_once_with({'thing1': ''}, variables)
 
 
-    def test_check_any_conditions_with_all_false(self):
+    @patch.object(engine, 'check_condition', return_value=False)
+    def test_check_any_conditions_with_all_false(self, *args):
         conditions = {'any': [{'thing1': ''}, {'thing2': ''}]}
         variables = BaseVariables()
-
-        engine.check_condition = MagicMock(return_value=False)
 
         result = engine.check_conditions_recursively(conditions, variables)
         self.assertEqual(result, False)
@@ -155,14 +147,31 @@ class EngineTests(TestCase):
     ###
     def test_check_operator_comparison(self):
         string_type = StringType('yo yo')
-        string_type.contains = MagicMock(return_value=True)
-        result = engine._do_operator_comparison(string_type, 'contains', 'its mocked')
-        self.assertTrue(result)
-        string_type.contains.assert_called_once_with('its mocked')
+        with patch.object(string_type, 'contains', return_value=True):
+            result = engine._do_operator_comparison(
+                    string_type, 'contains', 'its mocked')
+            self.assertTrue(result)
+            string_type.contains.assert_called_once_with('its mocked')
 
 
     ###
     ### Actions
     ###
-    def test_do_action(self):
-        pass
+    def test_do_actions(self):
+        actions = [ {'name': 'action1'},
+                    {'name': 'action2',
+                     'params': {'param1': 'foo', 'param2': 10}}]
+        defined_actions = BaseActions()
+        defined_actions.action1 = MagicMock()
+        defined_actions.action2 = MagicMock()
+
+        engine.do_actions(actions, defined_actions)
+
+        defined_actions.action1.assert_called_once_with()
+        defined_actions.action2.assert_called_once_with(param1='foo', param2=10)
+
+    def test_do_with_invalid_action(self):
+        actions = [{'name': 'fakeone'}]
+        err_string = "Action fakeone is not defined in class BaseActions"
+        with self.assertRaisesRegexp(AssertionError, err_string):
+            engine.do_actions(actions, BaseActions())

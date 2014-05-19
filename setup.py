b8a8e5a67ed5447d192739cb350ad8b49da571c4
Greg Einfrank <greg.einfrank@venmo.com>
2014-05-19 16:04:40 -0400

    Add url and license to setup.py

--- setup.py
+++ setup.py
@@ -10,5 +10,7 @@ setuptools.setup(
         description='Python DSL for setting up business intelligence rules that can be configured without code',
         author='Venmo',
         author_email='open-source@venmo.com',
+        url='https://github.com/venmo/business-rules',
         packages=['business_rules'],
+        license='MIT'
 )

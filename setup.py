2bde04bdd2a8e9ef54482f78bc55683e785efb79
Joel Bixby <joel.bixby@venmo.com>
2016-03-16 11:32:14 -0400

    Dynamiclly add the history to the setup description

--- setup.py
+++ setup.py
@@ -4,10 +4,15 @@ import setuptools
 
 from business_rules import __version__ as version
 
+with open('History.rst') as f:
+    history = f.read()
+
+description = 'Python DSL for setting up business intelligence rules that can be configured without code'
+
 setuptools.setup(
         name='business-rules',
         version=version,
-        description='Python DSL for setting up business intelligence rules that can be configured without code',
+        description='{0}\n\n{1}'.format(description, history),
         author='Venmo',
         author_email='open-source@venmo.com',
         url='https://github.com/venmo/business-rules',

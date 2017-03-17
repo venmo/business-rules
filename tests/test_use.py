c1d07eba50359ed34194c68f66120a567c47d2ab
David Morton <davidlmorton@gmail.com>
2017-03-17 09:54:03 -0500

    Add condition_aliases feature

--- tests/test_use.py
+++ tests/test_use.py
@@ -47,3 +47,113 @@ class UseCaseTests(TestCase):
             stop_on_first_trigger=False
         )
         self.assertEquals(actions.calls, [{'function': 'some_number_action', 'some_number': 5}])
+
+    def test_single_alias_condition(self):
+        aliases = {
+            'foo_is_foo': {'name': 'foo', 'operator': 'equal_to', 'value': 'foo'}
+        }
+        rules = [
+            {
+                'conditions': 'foo_is_foo',
+                'actions': [
+                    {'name': 'some_string_action', 'params': {'some_string': 'bar'}},
+                ],
+            },
+        ]
+
+        actions = SomeActions()
+        results = run_all(
+            rule_list=rules,
+            defined_variables=SomeVariables(),
+            defined_actions=actions,
+            condition_aliases=aliases,
+            stop_on_first_trigger=False
+        )
+        self.assertEquals(actions.calls, [{'function': 'some_string_action', 'some_string': 'bar'}])
+
+    def test_multiple_alias_condition(self):
+        aliases = {
+            'foo_is_foo': {'name': 'foo', 'operator': 'equal_to', 'value': 'foo'},
+            '10_is_9': {'name': 'ten', 'operator': 'equal_to', 'value': 0}
+        }
+        rules = [
+            {
+                'conditions': 'foo_is_foo',
+                'actions': [
+                    {'name': 'some_string_action', 'params': {'some_string': 'bar'}},
+                ],
+            },
+            {
+                'conditions': '10_is_9',
+                'actions': [
+                    {'name': 'some_number_action', 'params': {'some_number': 109}},
+                ],
+            },
+        ]
+
+        actions = SomeActions()
+        results = run_all(
+            rule_list=rules,
+            defined_variables=SomeVariables(),
+            defined_actions=actions,
+            condition_aliases=aliases,
+            stop_on_first_trigger=False
+        )
+        self.assertEquals(actions.calls, [{'function': 'some_string_action', 'some_string': 'bar'}])
+
+    def test_complex_condition_using_aliases(self):
+        aliases = {
+            'foo_is_foo': {'name': 'foo', 'operator': 'equal_to', 'value': 'foo'},
+            '10_is_9': {'name': 'ten', 'operator': 'equal_to', 'value': 0}
+        }
+        rules = [
+            {
+                'conditions': {
+                    'any': [
+                        '10_is_9',
+                        {
+                            'all': [
+                                'foo_is_foo',
+                                {'name': 'ten', 'operator': 'greater_than', 'value': 0}
+                            ],
+                        },
+                    ],
+                },
+                'actions': [
+                    {'name': 'some_string_action', 'params': {'some_string': 'bar'}},
+                ],
+            },
+        ]
+
+        actions = SomeActions()
+        results = run_all(
+            rule_list=rules,
+            defined_variables=SomeVariables(),
+            defined_actions=actions,
+            condition_aliases=aliases,
+            stop_on_first_trigger=False
+        )
+        self.assertEquals(actions.calls, [{'function': 'some_string_action', 'some_string': 'bar'}])
+
+    def test_missing_alias(self):
+        aliases = {
+            'foo_is_foo': {'name': 'foo', 'operator': 'equal_to', 'value': 'foo'}
+        }
+        rules = [
+            {
+                'conditions': 'missing_condition_alias',
+                'actions': [
+                    {'name': 'some_string_action', 'params': {'some_string': 'bar'}},
+                ],
+            },
+        ]
+
+        actions = SomeActions()
+        with self.assertRaises(RuntimeError):
+            results = run_all(
+                rule_list=rules,
+                defined_variables=SomeVariables(),
+                defined_actions=actions,
+                condition_aliases=aliases,
+                stop_on_first_trigger=False
+            )

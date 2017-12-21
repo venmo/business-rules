f4e6b80d6353e90018ee73063fd4b9833008c8d5
jaydeepsinh <jaydeep@awaaz.de>
2017-12-21 23:10:21 +0530

    Modified package name

--- setup.py
+++ setup.py
@@ -10,12 +10,12 @@ with open('HISTORY.rst') as f:
 description = 'Python DSL for setting up business intelligence rules that can be configured without code'
 
 setuptools.setup(
-        name='business-rules',
+        name='business-rules-bulk',
         version=version,
         description='{0}\n\n{1}'.format(description, history),
         author='Venmo',
         author_email='open-source@venmo.com',
         url='https://github.com/venmo/business-rules',
-        packages=['business_rules'],
+        packages=['business_rules_bulk'],
         license='MIT'
 )

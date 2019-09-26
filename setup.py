4eb5584a758b0b8e2ecfb1103c6022d2ced9936a
Matt Layman <matthewlayman@gmail.com>
2019-09-26 08:48:33 -0700

    Update docs and tests to refence recent Python 3 versions.

--- setup.py
+++ setup.py
@@ -17,5 +17,16 @@ setuptools.setup(
         author_email='open-source@venmo.com',
         url='https://github.com/venmo/business-rules',
         packages=['business_rules'],
-        license='MIT'
+        license='MIT',
+        classifier=[
+            "Programming Language :: Python :: 2.6",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 3.2",
+            "Programming Language :: Python :: 3.3",
+            "Programming Language :: Python :: 3.4",
+            "Programming Language :: Python :: 3.5",
+            "Programming Language :: Python :: 3.6",
+            "Programming Language :: Python :: 3.7",
+            "Programming Language :: Python :: Implementation :: PyPy",
+        ]
 )

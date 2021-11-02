1d45b42f648a08f8565b05a1efb8596cbe3d70f8
Aleksei_Furmenkov <aleksei_furmenkov@epam.com>
2021-11-02 11:30:15 +0200

    wip

--- business_rules/operators.py
+++ business_rules/operators.py
@@ -559,14 +559,10 @@ class DataframeType(BaseType):
     @type_operator(FIELD_DATAFRAME)
     def is_not_unique(self, other_value):
         target = other_value.get("target")
-        unique_target = self.value[target].duplicated()
-        self.value[f"result_{uuid4()}"] = unique_target
         comparator = other_value.get("comparator")
-        if comparator:
-            unique_comparator = self.value[comparator].duplicated()
-            self.value[f"result_{uuid4()}"] = unique_comparator
-            return (True in unique_target.values) or (True in unique_comparator.values)
-        return True in unique_target.values
+        no_dupl_rows_df = self.value.drop_duplicates()
+        no_dupl_rows_df[[target, comparator]].groupby(target).count()
+        no_dupl_rows_df[[target, comparator]].groupby(comparator).count()
 
 @export_type
 class GenericType(SelectMultipleType, SelectType, StringType, NumericType, BooleanType, DataframeType):

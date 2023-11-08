47cb4400a5d78a0c823a049e24c86bbbfbf8c3e1
Nick Krug <nick.krug@greenphire.com>
2023-11-08 15:22:49 -0500

    Fix description to no longer have newlines

--- setup.py
+++ setup.py
@@ -4,15 +4,18 @@ import setuptools
 
 from business_rules import __version__ as version
 
+with open('README.mdt') as f:
+    readme = f.read()
+
 with open('HISTORY.rst') as f:
     history = f.read()
-
-description = 'Python DSL for setting up business intelligence rules that can be configured without code'
 
 setuptools.setup(
         name='business-rules',
         version=version,
-        description='{0}\n\n{1}'.format(description, history),
+        description='Python DSL for setting up business intelligence rules that can be configured without code',
+        long_description='{0}\n\n{1}'.format(readme, history),
+        long_description_content_type='text/markdown',
         author='Venmo',
         author_email='open-source@venmo.com',
         url='https://github.com/venmo/business-rules',
